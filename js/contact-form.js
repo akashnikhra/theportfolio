ffb9bb79cfc691a18683475b2902bf117fb3c567
akashnikhra <akashnikhra@gmail.com>
2015-06-03 03:33:37 +0530

    11th

---
+++ js/contact-form.js
@@ -0,0 +1,38 @@
+/* ---------------------------------------------
+ Contact form
+ --------------------------------------------- */
+$(document).ready(function(){
+    $("#submit_btn").click(function(){
+        
+        //get input field values
+        var user_name = $('input[name=name]').val();
+        var user_email = $('input[name=email]').val();
+        var user_message = $('textarea[name=message]').val();
+        
+        //simple validation at client's end
+        //we simply change border color to red if empty field using .css()
+        var proceed = true;
+        if (user_name == "") {
+            $('input[name=name]').css('border-color', '#e41919');
+            proceed = false;
+        }
+        if (user_email == "") {
+            $('input[name=email]').css('border-color', '#e41919');
+            proceed = false;
+        }
+        
+        if (user_message == "") {
+            $('textarea[name=message]').css('border-color', '#e41919');
+            proceed = false;
+        }
+        
+        return false;
+    });
+    
+    //reset previously set border colors and hide all message on .keyup()
+    $("#contact_form input, #contact_form textarea").keyup(function(){
+        $("#contact_form input, #contact_form textarea").css('border-color', '');
+        $("#result").slideUp();
+    });
+    
+});
